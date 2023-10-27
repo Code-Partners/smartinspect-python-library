f015c64da43e30b4f973d06d6d9e875d62ff09a7
Sergey Udachin <sergey.udachin@code-partners.com>
2023-10-27 19:32:21 +0300

    Add compile functions to BinaryFormatter

--- formatters.py
+++ formatters.py
@@ -1,16 +1,32 @@
+from abc import ABC, abstractmethod
 import sys
 import struct
 from io import BytesIO
 from enum import Enum
-from packets import Packet, LogEntry
+from packets import Packet, LogEntry, ProcessFlow, Watch, ControlCommand
 from packet_type import PacketType
 from log_entry_type import LogEntryType
 from viewer_type import ViewerType
+from color import Color
 
 
-class Formatter:
-    def __init__(self):
+class Formatter(ABC):
+    """Responsible for formatting and writing a packet. """
+
+    @abstractmethod
+    def compile(self, packet: Packet) -> int:
+        """Compiles a packet and returns the required size for the compiled result"""
         pass
+
+    @abstractmethod
+    def write(self, stream) -> None:
+        """Writes a compiled packet to a supplied stream"""
+
+    @abstractmethod
+    def format(self, packet: Packet, stream):
+        """Compiles a packet and writes it to a stream"""
+        self.compile(packet)
+        self.write(stream)
 
 
 class BinaryFormatter(Formatter):
@@ -27,15 +43,17 @@ class BinaryFormatter(Formatter):
 
     def __init__(self):
         super().__init__()
-        self.__buffer = None
-        self.__stream: BytesIO | None = None
+        self.__buffer = bytearray()
+        self.__stream: BytesIO = BytesIO()
         self.__size: int = 0
         self.__packet: Packet | None = None
 
-    # if-else fork was removed here because
-    # there seems to be no profit in clearing BytesIO buffer over creating a new one
+    # There is no evident profit in resetting position in BytesIO buffer over creating a new one
     def __reset_stream(self):
-        self.__stream = BytesIO()
+        if self.__size > self.__MAX_STREAM_CAPACITY:
+            self.__stream = BytesIO()
+        else:
+            self.__stream.seek(0)
 
     def compile(self, packet: Packet) -> int:
         self.__reset_stream()
@@ -49,10 +67,12 @@ class BinaryFormatter(Formatter):
 
     def __compile_log_entry(self):
         log_entry: LogEntry = self.__packet
+
         app_name = self.__encode_string(log_entry.get_app_name())
         session_name = self.__encode_string(log_entry.get_session_name())
         title = self.__encode_string(log_entry.get_title())
         host_name = self.__encode_string(log_entry.get_host_name())
+
         self.__write_enum(log_entry.get_log_entry_type())
         self.__write_enum(log_entry.get_viewer_type())
         self.__write_length(app_name)
@@ -64,7 +84,43 @@ class BinaryFormatter(Formatter):
         self.__write_int(log_entry.get_thread_id())
         self.__write_timestamp(log_entry.get_timestamp())
         self.__write_color(log_entry.get_color())
+
+        self.__write_data(app_name)
+        self.__write_data(session_name)
+        self.__write_data(title)
+        self.__write_data(host_name)
+        self.__write_data(log_entry.get_data())
         # print(int.from_bytes(self.__stream.getvalue(), "little", signed=True))
+
+    def __compile_process_flow(self) -> None:
+        process_flow: ProcessFlow = self.__packet
+
+        title = self.__encode_string(process_flow.get_title())
+        host_name = self.__encode_string(process_flow.get_host_name())
+
+        self.__write_enum(process_flow.get_process_flow_type())
+        self.__write_length(title)
+        self.__write_length(host_name)
+        self.__write_int(process_flow.get_process_id())
+        self.__write_int(process_flow.get_thread_id())
+        self.__write_timestamp(process_flow.get_timestamp())
+
+        self.__write_data(title)
+        self.__write_data(host_name)
+
+    def __compile_watch(self) -> None:
+        watch: Watch = self.__packet
+
+        name = self.__encode_string(watch.get_name())
+        value = self.__encode_string(watch.get_value())
+
+        self.__write_length(name)
+        self.__write_length(value)
+        self.__write_enum(watch.get_watch_type())
+        self.__write_timestamp(watch.get_timestamp())
+
+        self.__write_data(name)
+        self.__write_data(value)
 
     @staticmethod
     def __encode_string(value: str) -> bytearray:
@@ -90,7 +146,7 @@ class BinaryFormatter(Formatter):
     def __write_timestamp(self, value: int) -> None:
         timestamp = value / self.__MICROSECONDS_PER_DAY + self.__DAY_OFFSET
         timestamp += (value % self.__MICROSECONDS_PER_DAY) / self.__MICROSECONDS_PER_DAY
-        self.__write_float(timestamp)
+        self.__write_double(timestamp)
 
     # It goes under 'double' in Java code but seems to be correctly processed by 'float' in Python
     def __write_float(self, value: float):
@@ -102,6 +158,13 @@ class BinaryFormatter(Formatter):
         content: bytes = log_header.get_content().encode('utf-8')
         self.__write_length(content)
         self.__write_data(content)
+
+    def __compile_control_command(self) -> None:
+        control_command: ControlCommand = self.__packet
+
+        self.__write_enum(control_command.get_control_command_type())
+        self.__write_length(control_command.get_data())
+        self.__write_data(control_command.get_data())
 
     def __write_length(self, content: bytes) -> None:
         if isinstance(content, bytes):
@@ -119,7 +182,23 @@ class BinaryFormatter(Formatter):
         else:
             color = value.get_red() | value.get_green() << 8 | value.get_blue() << 16 | value.get_alpha() << 24
         # converting to unsigned int and writing it
-        self.__write_int(color - 2 ** 32)
+        unsigned_int = struct.unpack("<i", struct.pack("<I", color))[0]
+        self.__write_int(unsigned_int)
+
+    def format(self, packet):
+        self.compile(packet)
+        return self.__stream.getvalue()
+
+    def write(self, stream):
+        if self.__size > 0:
+            self.__write_int(self.__packet.get_packet_type().value)
+
+    def __write_double(self, value: float) -> None:
+        long_bits = struct.pack("<d", value)
+        self.__write_long(long_bits)
+
+    def __write_long(self, long_bits):
+        self.__stream.write(long_bits)
 
 
 if __name__ == "__main__":
--- tcp_protocol.py
+++ tcp_protocol.py
@@ -14,13 +14,13 @@ class TcpProtocol(Protocol):
     __BUFFER_SIZE = 0x2000
     __CLIENT_BANNER = bytearray(f"SmartInspect Java Library v{SmartInspect.get_version()}\n", encoding="UTF-8")
     __ANSWER_SIZE = 2
-    __hostname = "127.0.0.1"
+    _hostname = "127.0.0.1"
     __timeout = 30000
-    __port = 4228
+    _port = 4228
 
     def __init__(self):
         super().__init__()
-        self.__answer: bytearray | None = None
+        self.__answer: bytearray = bytearray()
         self.__formatter = BinaryFormatter()
         self._load_options()
         self.__socket = None
@@ -35,8 +35,8 @@ class TcpProtocol(Protocol):
 
     def _build_options(self, builder: ConnectionsBuilder) -> None:
         super()._build_options(builder)
-        builder.add_option("host", self.__hostname)
-        builder.add_option("port", self.__port)
+        builder.add_option("host", self._hostname)
+        builder.add_option("port", self._port)
         builder.add_option("timeout", self.__timeout)
 
     def _load_options(self) -> None:
@@ -46,6 +46,7 @@ class TcpProtocol(Protocol):
         # self.__port = self._get_integer_option("port", 4228)
 
     def __do_handshake(self):
+
         server_banner = "".encode()
         while True:
             current_byte = self.__socket.recv(1)
@@ -60,19 +61,19 @@ class TcpProtocol(Protocol):
 
         client_banner = self.__CLIENT_BANNER
         self.__socket.sendall(client_banner)
-        self.__socket.close()
+
 
     def _internal_connect(self):
         socket_ = self.__socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         socket_.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
         socket_.settimeout(self.__timeout)
         try:
-            socket_.connect((self.__hostname, self.__port))
+            socket_.connect((self._hostname, self._port))
             self.__do_handshake()
             self._internal_write_log_header()
         except ConnectionError as e:
             raise SmartInspectException(f"There was a connection error. \n"
-                                        f"Check if SI Console is running on {self.__hostname}:{self.__port} \n"
+                                        f"Check if SI Console is running on {self._hostname}:{self._port} \n"
                                         f"Your system returned: {e.errno}: {e.strerror}")
         except SmartInspectException as e:
             raise e
@@ -80,13 +81,25 @@ class TcpProtocol(Protocol):
     def _internal_disconnect(self) -> None:
         self.__socket.close()
 
-    # def _internal_write_packet(self, packet: Packet) -> None:
-    #     self.__formatter.format(packet, output_stream)
-    #     self.__socket.sendall(packet)
-    #
-    #     if len(answer) != 2:
-    #         raise SmartInspectException(
-    #             "Could not read server answer correctly: Connection has been closed unexpectedly")
+    def _internal_write_packet(self, packet: Packet) -> None:
+        stream = self.__formatter.format(packet)
+        self.__socket.sendall(stream)
+        server_answer = "".encode()
+        # server_answer += self.__socket.recv(1)
+        # while True:
+        #     current_byte = self.__socket.recv(1)
+        #     if current_byte == b'\n':
+        #         break
+        #     if not current_byte:
+        #         raise SmartInspectException(
+        #             "Could not read server banner correctly: " +
+        #             "Connection has been closed unexpectedly")
+        #     server_answer += current_byte
+        # print(server_answer)
+        if len(server_answer) != self.__ANSWER_SIZE:
+            raise SmartInspectException(
+                "Could not read server answer correctly: Connection has been closed unexpectedly")
+        self.__socket.close()
 
 
 if __name__ == '__main__':
