bca2e6eb6f301461388ff649f5fe49110c068bf2
Sergey Udachin <sergey.udachin@code-partners.com>
2023-11-09 16:29:52 +0300

    feat: add PacketQueue(partial)

--- packets/packet_queue.py
+++ packets/packet_queue.py
@@ -1,5 +1,72 @@
 # Copyright (C) Code Partners Pty. Ltd. All rights reserved. #
+from packets.packet import Packet
+from packets.log_entry import LogEntry
+from packets.log_entry.log_entry_type import LogEntryType
+from common.viewer_id import ViewerId
+
+
+class PacketQueueItem:
+    def __init__(self, packet: Packet):
+        self.packet = packet
+        self.next = None
+        self.prev = None
+
 
 class PacketQueue:
-    pass
+    __OVERHEAD = 24
+
+    def __init__(self):
+        self.__backlog: int = 0
+        self.__size: int = 0
+        self.__count: int = 0
+        self.__head: (Packet, None) = None
+        self.__tail: (Packet, None) = None
+
+    def push(self, packet: Packet) -> None:
+        item = PacketQueueItem(packet)
+
+        if self.__tail is None:
+            self.__head = item
+            self.__tail = item
+        else:
+            self.__tail.next = item
+            item.prev = self.__tail
+            self.__tail = item
+
+        self.__count += 1
+        self.__size += packet.get_size() + self.__OVERHEAD
+        # self.__resize()
+
+    def pop(self) -> (Packet, None):
+        item = self.__head
+
+        if item is None:
+            return None
+
+        packet = item.packet
+        self.__head = item.next
+
+        if self.__head is not None:
+            self.__head.prev = None
+        else:
+            self.__tail = None
+
+        self.__count -= 1
+        self.__size -= packet.get_size() + self.__OVERHEAD
+        return packet
+
+    def clear(self) -> None:
+        while self.pop() is not None:
+            ...
+
+
+if __name__ == '__main__':
+    pq = PacketQueue()
+    pq.push(LogEntry(LogEntryType.MESSAGE, ViewerId.NO_VIEWER))
+    pq.push(LogEntry(LogEntryType.MESSAGE, ViewerId.NO_VIEWER))
+    pq.push(LogEntry(LogEntryType.MESSAGE, ViewerId.NO_VIEWER))
+    pq.push(LogEntry(LogEntryType.MESSAGE, ViewerId.NO_VIEWER))
+    pq.clear()
+
+
 
