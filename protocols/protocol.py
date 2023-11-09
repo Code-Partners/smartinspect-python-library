87897d2992a200a94a72224de729f47607bd3f59
Sergey Udachin <sergey.udachin@code-partners.com>
2023-11-09 16:33:02 +0300

    feat: update Protocol

--- protocols/protocol.py
+++ protocols/protocol.py
@@ -7,15 +7,25 @@ from common.lookup_table import LookupTable
 from packets.packet_queue import PacketQueue
 from packets.packet import Packet
 from packets.log_header import LogHeader
+from common import Level
+from common.exceptions import ProtocolException
+from common.events import ErrorEvent
 
 
 class Protocol(ABC):
     def __init__(self):
         self.__lock: threading.Lock = threading.Lock()
-        self.options = LookupTable()
+        self.__options = LookupTable()
         self.__queue = PacketQueue()
-        self.listeners = dict()
-        self._app_name = ""
+        self.__listeners = dict()
+        self.__app_name = ""
+        self.__hostname = ""
+        self.__level = Level.MESSAGE
+        self.__async_enabled = False
+        self.__scheduler = None
+        self.__connected = False
+        self.__reconnect = False
+        self.__keep_open = False
 
     @staticmethod
     @abstractmethod
@@ -26,10 +36,10 @@ class Protocol(ABC):
         pass
 
     def _get_string_option(self, key: str, default_value: str) -> str:
-        return self.options.get_string_value(key, default_value)
+        return self.__options.get_string_value(key, default_value)
 
     def _get_integer_option(self, key: str, default_value: int) -> int:
-        return self.options.get_integer_value(key, default_value)
+        return self.__options.get_integer_value(key, default_value)
 
     def _is_valid_option(self, option_name: str) -> bool:
         is_valid = bool(option_name in ("caption",
@@ -52,8 +62,8 @@ class Protocol(ABC):
 
     def _internal_write_log_header(self):
         log_header: LogHeader = LogHeader()
-        log_header.set_hostname(self._hostname)
-        log_header.set_app_name(self._app_name)
+        log_header.set_hostname(self.__hostname)
+        log_header.set_app_name(self.__app_name)
         self._internal_write_packet(log_header)
 
     @abstractmethod
@@ -94,7 +104,7 @@ class Protocol(ABC):
                     self._reset()
                     raise exception
             except Exception as exception:
-                self.__handle_exception(exception)
+                self._handle_exception(exception)
 
     def _reset(self):
         self.__connected = False
@@ -113,24 +123,21 @@ class Protocol(ABC):
         else:
             self.__queue.clear()
 
-    def _handle_exception(self, exception):
-        pass
 
     def _is_asynchronous(self) -> bool:
         return self.__async_enabled
 
     def write_packet(self, packet: Packet):
-        ...
-        # with self.__lock:
-        #     if packet.get_level.value < self.__level.value:
-        #         return
-        #
-        #     if self.__async_enabled:
-        #         if self.__scheduler is None:
-        #             return
-        #         self.__schedule_write_packet(packet)
-        #     else:
-        #         self.__do_write_packet(packet)
+        with self.__lock:
+            if packet.get_level().value < self.__level.value:
+                return
+
+            if self.__async_enabled:
+                if self.__scheduler is None:
+                    return
+                self.__schedule_write_packet(packet)
+            else:
+                self._impl_write_packet(packet)
 
     def __schedule_write_packet(self, packet: Packet) -> None:
         ...
@@ -138,3 +145,40 @@ class Protocol(ABC):
         # command.set_action(SchedulerAction.WritePacket)
         # command.set_state(packer)
         # self.__scheduler.schedule(command)
+
+    def _impl_write_packet(self, packet: Packet) -> None:
+        if (
+                not self.__connected and
+                not self.__reconnect and
+                self.__keep_open):
+            return
+
+        level = packet.get_level()
+
+        try:
+            try:
+                ...
+            except Exception as e:
+                self._reset()
+                raise e
+        except Exception as e:
+            self._handle_exception(e.args[0])
+
+    def _handle_exception(self, message: str):
+        self.__failed = True
+        protocol_exception = ProtocolException(message)
+        protocol_exception.set_protocol_name(self._get_name())
+        protocol_exception.set_protocol_options(self._get_options())
+
+        if self.__async_enabled:
+            self._do_error(protocol_exception)
+        else:
+            raise protocol_exception
+
+    def _do_error(self, exception: Exception):
+        #add lock
+        error_event = ErrorEvent(self, exception)
+        for listener in self.__listeners:
+            listener.on_error(error_event)
+
+
