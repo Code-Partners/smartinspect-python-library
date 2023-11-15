97c42d06144ca9f040c783c108b30d41d2961332
Sergey Udachin <sergey.udachin@code-partners.com>
2023-11-15 12:53:54 +0300

    feat: Add ControlCommandEvent, LogEntryEvent, ProcessFlowEvent, WatchEvent

--- common/events/control_command_event.py
+++ common/events/control_command_event.py
@@ -1,2 +1,20 @@
+from packets.control_command import ControlCommand
+
+
 class ControlCommandEvent:
-    ...
+    def __init__(self, source: object, control_command: ControlCommand):
+        """Initializes a ControlCommandEvent instance.
+
+        :param source: the source object which fired the event
+        :param control_command: the ControlCommand packet which caused the event"""
+        self.__set_source(source)
+        self.__control_command: ControlCommand = control_command
+
+    @property
+    def log_entry(self) -> ControlCommand:
+        return self.__control_command
+
+    def __set_source(self, source: object):
+        if source is None:
+            raise ValueError("Source is None")
+        self.__source = source
--- common/events/watch_event.py
+++ common/events/watch_event.py
@@ -1,2 +1,20 @@
+from packets.watch import Watch
+
+
 class WatchEvent:
-    ...
+    def __init__(self, source: object, watch: Watch):
+        """Initializes a WatchEvent instance.
+
+        :param source: the source object which fired the event
+        :param watch: the Watch packet which caused the event"""
+        self.__set_source(source)
+        self.__watch: Watch = watch
+
+    @property
+    def watch(self) -> Watch:
+        return self.__watch
+
+    def __set_source(self, source: object):
+        if source is None:
+            raise ValueError("Source is None")
+        self.__source = source
