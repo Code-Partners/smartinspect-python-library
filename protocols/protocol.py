a361dc4bc276c18369a9f00856c91ff4a06b8536
Sergey Udachin <sergey.udachin@code-partners.com>
2023-11-15 13:12:05 +0300

    feat: Update Protocol methods

--- protocols/protocol.py
+++ protocols/protocol.py
@@ -4,12 +4,17 @@ import threading
 import time
 from abc import ABC, abstractmethod
 from common.lookup_table import LookupTable
+from common.protocol_command import ProtocolCommand
+from common.protocol_listener import ProtocolListener
+from common.scheduler_action import SchedulerAction
+from connections.builders import ConnectionsBuilder
 from packets.packet_queue import PacketQueue
 from packets.packet import Packet
 from packets.log_header import LogHeader
 from common.level import Level
 from common.exceptions import ProtocolException
 from common.events.error_event import ErrorEvent
+from scheduler_command import SchedulerCommand
 
 
 class Protocol(ABC):
@@ -17,7 +22,7 @@ class Protocol(ABC):
         self.__lock: threading.Lock = threading.Lock()
         self.__options = LookupTable()
         self.__queue = PacketQueue()
-        self.__listeners = dict()
+        self.__listeners = set()
         self.__app_name = ""
         self.__hostname = ""
         self.__level = Level.MESSAGE
@@ -26,6 +31,8 @@ class Protocol(ABC):
         self.__connected = False
         self.__reconnect = False
         self.__keep_open = False
+        self.__caption = ""
+        self.__initialized = False
 
     @staticmethod
     @abstractmethod
@@ -91,10 +98,10 @@ class Protocol(ABC):
             if self.__async_enabled:
                 if self.__scheduler is not None:
                     return
-                
+
                 self.__start_scheduler()
                 self.__schedule_connect()
-            
+
             self._impl_connect()
 
     def disconnect(self) -> None:
@@ -112,7 +119,7 @@ class Protocol(ABC):
                     self._reset()
                     raise exception
             except Exception as exception:
-                self._handle_exception(exception)
+                self._handle_exception(exception.args[0])
 
     def _reset(self):
         self.__connected = False
@@ -127,10 +134,9 @@ class Protocol(ABC):
             try:
                 self._reset()
             except Exception as exception:
-                self._handle_exception(exception)
+                self._handle_exception(exception.args[0])
         else:
             self.__queue.clear()
-
 
     def _is_asynchronous(self) -> bool:
         return self.__async_enabled
@@ -165,7 +171,19 @@ class Protocol(ABC):
 
         try:
             try:
-                ...
+                skip = False
+                if self.__backlog_enabled:
+                    if (level.value >= self.__backlog_flushon.value and
+                            level != Level.CONTROL
+                    ):
+                        self.__flush_queue()
+                    else:
+                        self.__queue.push(packet)
+                        skip = True
+
+                if not skip:
+                    self.__forward_packet(packet, not self.__keep_open)
+
             except Exception as e:
                 self._reset()
                 raise e
@@ -176,7 +194,7 @@ class Protocol(ABC):
         self.__failed = True
         protocol_exception = ProtocolException(message)
         protocol_exception.set_protocol_name(self._get_name())
-        protocol_exception.set_protocol_options(self._get_options())
+        protocol_exception.set_protocol_options(self.__get_options())
 
         if self.__async_enabled:
             self._do_error(protocol_exception)
@@ -184,7 +202,7 @@ class Protocol(ABC):
             raise protocol_exception
 
     def _do_error(self, exception: Exception):
-        #add lock
+        # add lock
         error_event = ErrorEvent(self, exception)
         for listener in self.__listeners:
             listener.on_error(error_event)
@@ -195,4 +213,80 @@ class Protocol(ABC):
     def __schedule_connect(self):
         pass
 
+    def get_caption(self) -> str:
+        return self.__caption
 
+    def dispatch(self, command: ProtocolCommand):
+        with self.__lock:
+            if self.__async_enabled:
+                if self.__scheduler is None:
+                    return None
+
+                self.__schedule_dispatch(command)
+
+            else:
+                self.__impl_dispatch(command)
+
+    def __schedule_dispatch(self, command: ProtocolCommand) -> None:
+        scheduler_command = SchedulerCommand()
+        scheduler_command.set_action(SchedulerAction.DISPATCH)
+        scheduler_command.set_state(command)
+
+        self.__scheduler.schedule(scheduler_command)
+
+    def __impl_dispatch(self, command: ProtocolCommand):
+        if self.__connected:
+            try:
+                self._internal_dispatch(command)
+            except Exception as e:
+                self._handle_exception(e.args[0])
+
+    def _internal_dispatch(self, command: ProtocolCommand):
+        """Empty by default"""
+        ...
+
+    def __get_options(self) -> str:
+        builder = ConnectionsBuilder()
+        self._build_options(builder)
+
+        return builder.get_connections()
+
+    def initialize(self, options: str):
+        # TODO full implementation (this is only a stub)
+        # with self.__lock:
+        self.__initialized = True
+
+    def add_listener(self, listener: ProtocolListener):
+        # TODO implement lock
+        if isinstance(listener, ProtocolListener):
+            self.__listeners.add(listener)
+
+    @abstractmethod
+    def _internal_disconnect(self) -> None:
+        pass
+
+    def __flush_queue(self) -> None:
+        packet = self.__queue.pop()
+        while packet is not None:
+            self.__forward_packet(packet, False)
+            packet = self.__queue.pop()
+
+    def __forward_packet(self, packet: Packet, disconnect: bool) -> None:
+        if not self.__connected:
+            if not self.__keep_open:
+                self._internal_connect()
+                self.__connected = True
+                self.__failed = False
+            else:
+                self.__reconnect()
+
+        if self.__connected:
+            packet.lock()
+            try:
+                self._internal_write_packet(packet)
+            finally:
+                packet.unlock()
+
+            if disconnect:
+                self.__connected = False
+                self._internal_disconnect()
