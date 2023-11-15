8f0632f74e29697aa50a3d783ab22c353dd9fa2f
Sergey Udachin <sergey.udachin@code-partners.com>
2023-11-15 13:08:08 +0300

    feat: add SessionDefaults

--- session/session_defaults.py
+++ session/session_defaults.py
@@ -1,14 +1,39 @@
+from common.color import Color
 from session.session import Session
 from common.level import Level
 
 
 class SessionDefaults:
     def __init__(self):
-        self.active = True
-        self.color = Session.DefaultColor
-        self.level = Level.DEBUG
+        self.__active: bool = True
+        self.__color: Color = Session.DEFAULT_COLOR
+        self.__level: Level = Level.DEBUG
 
     def _assign(self, session: Session) -> None:
-        session.set_active(self.active)
-        session.set_color(self.color)
-        session.set_level(self.level)
+        session.set_active(self.__active)
+        session.set_color(self.__color)
+        session.set_level(self.__level)
+
+    def is_active(self) -> bool:
+        return self.__active
+
+    def set_active(self, active: bool) -> None:
+        if not isinstance(active, bool):
+            raise TypeError("Active must be a boolean")
+        self.__active = active
+
+    def get_color(self) -> Color:
+        return self.__color
+
+    def set_color(self, color: Color) -> None:
+        if not isinstance(color, Color):
+            raise TypeError("Color must be a Color")
+        self.__color = color
+
+    def get_level(self) -> Level:
+        return self.__level
+
+    def set_level(self, level: Level) -> None:
+        if not isinstance(level, Level):
+            raise TypeError("Level must be a Level")
+        self.__level = level
