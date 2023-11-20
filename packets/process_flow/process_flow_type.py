74160f872146f823d65cd05831db78359d71ebef
Sergey Udachin <sergey.udachin@code-partners.com>
2023-11-20 09:05:13 +0300

    refactor: turn names to uppercase with underscore as separator

--- packets/process_flow/process_flow_type.py
+++ packets/process_flow/process_flow_type.py
@@ -2,9 +2,9 @@ from enum import Enum
 
 
 class ProcessFlowType(Enum):
-    EnterMethod = 0
-    LeaveMethod = 1
-    EnterThread = 2
-    LeaveThread = 3
-    EnterProcess = 4
-    LeaveProcess = 5
+    ENTER_METHOD = 0
+    LEAVE_METHOD = 1
+    ENTER_THREAD = 2
+    LEAVE_THREAD = 3
+    ENTER_PROCESS = 4
+    LEAVE_PROCESS = 5
