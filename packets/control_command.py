d6245b5130e6f2634c44e43c99d57191a0fcdddd
Sergey Udachin <sergey.udachin@code-partners.com>
2023-11-22 17:01:40 +0300

    feat: update `ControlCommand` implementation

--- packets/control_command.py
+++ packets/control_command.py
@@ -1,9 +1,37 @@
+from .control_command_type import ControlCommandType
 from .packet import Packet
+from .packet_type import PacketType
 
 
 class ControlCommand(Packet):
-    def get_data(self):
-        pass
+    __HEADER_SIZE = 8
 
-    def get_control_command_type(self):
-        pass
+    def __init__(self, command_type: ControlCommandType):
+        super().__init__()
+        self.control_command_type = command_type
+        self.data = bytes()
+
+    @property
+    def data(self) -> (bytes, bytearray):
+        return self.__data
+
+    @data.setter
+    def data(self, data: (bytes, bytearray)) -> None:
+        self.__data = data
+
+    @property
+    def control_command_type(self) -> ControlCommandType:
+        return self.__control_command_type
+
+    @control_command_type.setter
+    def control_command_type(self, command_type: ControlCommandType) -> None:
+        self.__control_command_type = command_type
+
+    @property
+    def packet_type(self) -> PacketType:
+        return PacketType.CONTROL_COMMAND
+
+    @property
+    def size(self):
+        return self.__HEADER_SIZE + len(self.data)
+
