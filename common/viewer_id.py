58592bdfe1a266c2cc8b8b73f3c0ad615bc4ac96
Sergey Udachin <sergey.udachin@code-partners.com>
2023-11-22 16:58:34 +0300

    feat: update enumerations list (seems full now)

--- common/viewer_id.py
+++ common/viewer_id.py
@@ -2,11 +2,24 @@ from enum import Enum
 
 
 class ViewerId(Enum):
-    METAFILE = 403
-    ICON = 402
-    JPEG = 401
-    BITMAP = 400
-    NO_VIEWER = -1
+    NO_VIEWER = - 1
     TITLE = 0
     DATA = 1
-    ...
+    LIST = 2
+    VALUE_LIST = 3
+    INSPECTOR = 4
+    TABLE = 5
+    WEB = 100
+    BINARY = 200
+    HTML_SOURCE = 300
+    JAVASCRIPT_SOURCE = 301
+    VBSCRIPT_SOURCE = 302
+    PERL_SOURCE = 303
+    SQL_SOURCE = 304
+    INI_SOURCE = 305
+    PYTHON_SOURCE = 306
+    XML_SOURCE = 307
+    BITMAP = 400
+    JPEG = 401
+    ICON = 402
+    METAFILE = 403
