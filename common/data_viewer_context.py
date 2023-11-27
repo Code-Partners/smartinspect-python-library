8d9e03c52d934c7032e0a7ae1d4245911f4b0030
Sergey Udachin <sergey.udachin@code-partners.com>
2023-11-27 17:49:02 +0300

    feat: add DataViewerContext

---
+++ common/data_viewer_context.py
@@ -0,0 +1,7 @@
+from common.text_context import TextContext
+from common.viewer_id import ViewerId
+
+
+class DataViewerContext(TextContext):
+    def __init__(self):
+        super().__init__(ViewerId.DATA)
