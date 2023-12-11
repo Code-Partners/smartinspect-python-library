e09d0d9fde9b7384f72b7d3decf2199c0c772b8d
Sergey Udachin <sergey.udachin@code-partners.com>
2023-12-11 09:52:10 +0300

    feat: significant update in `Protocol` methods and properties

--- protocols/protocol.py
+++ protocols/protocol.py
@@ -2,80 +2,82 @@
 
 import threading
 import time
-from abc import ABC, abstractmethod
+# from abc import ABC
+
+from common.events.error_event import ErrorEvent
+from common.exceptions import ProtocolException, SmartInspectException
+from common.file_rotate import FileRotate
+from common.level import Level
+from common.listener.protocol_listener import ProtocolListener
 from common.lookup_table import LookupTable
 from common.protocol_command import ProtocolCommand
-from common.listener.protocol_listener import ProtocolListener
-from common.scheduler_action import SchedulerAction
 from connections.builders import ConnectionsBuilder
 from connections.options_parser import OptionsParser
 from connections.options_parser_event import OptionsParserEvent
 from connections.options_parser_listener import OptionsParserListener
-from packets.packet_queue import PacketQueue
-from packets.packet import Packet
 from packets.log_header import LogHeader
-from common.level import Level
-from common.exceptions import ProtocolException, SmartInspectException
-from common.events.error_event import ErrorEvent
-from common.scheduler_command import SchedulerCommand
+from packets.packet import Packet
+from packets.packet_queue import PacketQueue
+from protocols.scheduler import Scheduler
+from protocols.scheduler_action import SchedulerAction
+from protocols.scheduler_command import SchedulerCommand
 
 
-class Protocol(ABC):
+class Protocol:
     def __init__(self):
         self.__lock: threading.Lock = threading.Lock()
         self.__options = LookupTable()
         self.__queue = PacketQueue()
         self.__listeners = set()
-        self.__app_name = ""
+        self.__appname = ""
         self.__hostname = ""
-        self.__level = Level.MESSAGE
-        self.__async_enabled = False
+        # self.__level = Level.MESSAGE
+        # self.__async_enabled = False
         self.__scheduler = None
         self.__connected = False
-        self.__reconnect = False
+        # self.__reconnect = False
         self.__keep_open = False
-        self.__caption = ""
+        # self.__caption = ""
         self.__initialized = False
-        self.__backlog_enabled = False
+        # self.__backlog_enabled = False
 
-    # def __create_options(self, options: str) -> None:
-    #     try:
-    #         parser = OptionsParser()
-    #         listener = OptionsParserListener()
-    #
-    #         def on_option(event: OptionsParserEvent):
-    #             self.__add_option(event.protocol, event.key, event.value)
-    #
-    #         listener.on_option = on_option
-    #         parser.parse(self._get_name(), options, listener)
-    #
-    #     except SmartInspectException as e:
-    #         self.__remove_options()
-    #         raise e
+    def __create_options(self, options: str) -> None:
+        try:
+            parser = OptionsParser()
+            listener = OptionsParserListener()
 
-    @staticmethod
-    @abstractmethod
-    def _get_name() -> str:
+            def on_option(event: OptionsParserEvent):
+                self.__add_option(event.protocol, event.key, event.value)
+
+            listener.on_option = on_option
+            parser.parse(self._get_name(), options, listener)
+
+        except SmartInspectException as e:
+            self.__remove_options()
+            raise e
+
+    def _get_name(self) -> str:
         pass
 
     def _load_options(self) -> None:
-        # self.__level = self._get_level_option("level", Level.DEBUG)
-        # self.__caption = self._get_string_option("caption", self._get_name())
-        # self.__reconnect = self._get_boolean_option("reconnect", False)
-        # self.__reconnect_interval = self._get_timespan_option("reconnect_interval", 0)
+        # pass
+        self.__level = self._get_level_option("level", Level.DEBUG)
+        self.__caption = self._get_string_option("caption", self._get_name())
+        self.__reconnect = self._get_boolean_option("reconnect", False)
+        self.__reconnect_interval = self._get_timespan_option("reconnect_interval", 0)
         #
-        # self.__backlog_enabled = self._get_boolean_option("backlog.enabled", False)
-        # self.__backlog_queue = self._get_size_option("backlog.queue", 2048)
-        # self.__backlog_flushon = self._get_level_option("backlog.flushon", Level.ERROR)
-        # self.__backlog__keepopen = self._get_boolean_option("backlog.keepopen", False)
+        self.__backlog_enabled = self._get_boolean_option("backlog.enabled", False)
+        self.__backlog_queue = self._get_size_option("backlog.queue", 2048)
+        self.__backlog_flushon = self._get_level_option("backlog.flushon", Level.ERROR)
+        self.__backlog_keep_open = self._get_boolean_option("backlog.keepopen", False)
         #
-        # # self.__queue.set_backlog(self.__backlog_queue)
-        # self.__keep_open = not self.__backlog_enabled or self.__backlog__keepopen
-        # self.__async_enabled = self._get_boolean_option("async.enabled", False)
-        # self.__async_throttle = self._get_boolean_option("async.throttle", True)
-        # self.__async_queue = self._get_size_option("async.queue", 2048)
-        # self.__async_clear_on_disconnect = self._get_boolean_option("async.clearondisconnect", False)
-        pass
+        # self.__queue.set_backlog(self.__backlog_queue)
+        # self.__keep_open = (not self.__backlog_enabled) or self.__backlog_keep_open
+        self.__async_enabled = self._get_boolean_option("async.enabled", False)
+        self.__async_throttle = self._get_boolean_option("async.throttle", True)
+        self.__async_queue = self._get_size_option("async.queue", 2048)
+        self.__async_clear_on_disconnect = self._get_boolean_option("async.clearondisconnect", False)
+
     def _get_string_option(self, key: str, default_value: str) -> str:
         return self.__options.get_string_value(key, default_value)
 
@@ -99,35 +101,56 @@ class Protocol(ABC):
         return is_valid
 
     def _build_options(self, builder: ConnectionsBuilder):
-        pass
-        # builder.add_option("async.enabled", self.__async_enabled)
-        # builder.add_option("async.clearondisconnect", self.__async_clearondisconnect)
+        # asynchronous options
+        builder.add_option("async.enabled", self.__async_enabled)
+        builder.add_option("async.clearondisconnect", self.__async_clear_on_disconnect)
+        builder.add_option("async.queue", int(self.__async_queue / 1024))
+        builder.add_option("async.throttle", self.__async_throttle)
+
+        # backlog options
+        builder.add_option("backlog.enabled", self.__backlog_enabled)
+        builder.add_option("backlog.flushon", self.__backlog_flushon)
+        builder.add_option("backlog.keepopen", self.__backlog_keep_open)
+        builder.add_option("backlog.queue", int(self.__backlog_queue / 1024))
+
+        # general options
+
+        builder.add_option("level", self.__level)
+        builder.add_option("caption", self.__caption)
+        builder.add_option("reconnect", self.__reconnect)
+        builder.add_option("reconnect.interval", int(self.__reconnect_interval))
 
     def _internal_write_log_header(self):
         log_header: LogHeader = LogHeader()
         log_header.hostname = self.__hostname
-        log_header.app_name = self.__app_name
+        log_header.appname = self.__appname
         self._internal_write_packet(log_header)
 
-    @abstractmethod
     def _internal_write_packet(self, packet: Packet):
         pass
 
-    @abstractmethod
     def _internal_connect(self):
         pass
 
-    def set_hostname(self, hostname: str) -> None:
-        self.__hostname = hostname
-
-    def get_hostname(self) -> str:
+    @property
+    def hostname(self) -> str:
         return self.__hostname
 
-    def get_appname(self) -> str:
-        return self.__app_name
+    @hostname.setter
+    def hostname(self, hostname: str) -> None:
+        if not isinstance(hostname, str):
+            raise TypeError("hostname must be an str")
+        self.__hostname = hostname
 
-    def set_appname(self, app_name: str) -> None:
-        self.__app_name = app_name
+    @property
+    def appname(self) -> str:
+        return self.__appname
+
+    @appname.setter
+    def appname(self, appname: str) -> None:
+        if not isinstance(appname, str):
+            raise TypeError("appname must be an str")
+        self.__appname = appname
 
     def connect(self):
         with self.__lock:
@@ -142,7 +165,17 @@ class Protocol(ABC):
 
     def disconnect(self) -> None:
         with self.__lock:
-            self.__perform_disconnect()
+            if self.__async_enabled:
+                if self.__scheduler is None:
+                    return
+
+                if self.__async_clear_on_disconnect:
+                    self.__scheduler.clear()
+
+                self.__schedule_disconnect()
+                self.__stop_scheduler()
+            else:
+                self.__impl_disconnect()
 
     def _impl_connect(self):
         if self.__keep_open and not self.__connected:
@@ -165,7 +198,7 @@ class Protocol(ABC):
         finally:
             self.__reconnect_tick_count = time.time() * 1000
 
-    def __perform_disconnect(self):
+    def __impl_disconnect(self):
         if self.__connected:
             try:
                 self._reset()
@@ -190,11 +223,10 @@ class Protocol(ABC):
                 self._impl_write_packet(packet)
 
     def __schedule_write_packet(self, packet: Packet) -> None:
-        ...
-        # command = SchedulerCommand()
-        # command.set_action(SchedulerAction.WritePacket)
-        # command.set_state(packer)
-        # self.__scheduler.schedule(command)
+        command = SchedulerCommand()
+        command.action = SchedulerAction.WRITE_PACKET
+        command.state = packet
+        self.__scheduler.schedule(command)
 
     def _impl_write_packet(self, packet: Packet) -> None:
         if (
@@ -210,8 +242,7 @@ class Protocol(ABC):
                 skip = False
                 if self.__backlog_enabled:
                     if (level.value >= self.__backlog_flushon.value and
-                            level != Level.CONTROL
-                    ):
+                            level != Level.CONTROL):
                         self.__flush_queue()
                     else:
                         self.__queue.push(packet)
@@ -238,16 +269,25 @@ class Protocol(ABC):
             raise protocol_exception
 
     def _do_error(self, exception: Exception):
-        # add lock
+        # TODO add lock
         error_event = ErrorEvent(self, exception)
         for listener in self.__listeners:
             listener.on_error(error_event)
 
     def __start_scheduler(self):
-        pass
+        self.__scheduler = Scheduler(self)
+        self.__scheduler.threshold = self.__async_queue
+        self.__throttle = self.__async_throttle
+        self.__scheduler.start()
 
-    def __schedule_connect(self):
-        pass
+    def __stop_scheduler(self):
+        self.__scheduler.stop()
+        self.__scheduler = None
+
+    def __schedule_connect(self) -> None:
+        command = SchedulerCommand()
+        command.action = SchedulerAction.CONNECT
+        self.__scheduler.schedule(command)
 
     def get_caption(self) -> str:
         return self.__caption
@@ -256,7 +296,7 @@ class Protocol(ABC):
         with self.__lock:
             if self.__async_enabled:
                 if self.__scheduler is None:
-                    return None
+                    return
 
                 self.__schedule_dispatch(command)
 
@@ -288,20 +328,28 @@ class Protocol(ABC):
         return builder.get_connections()
 
     def initialize(self, options: str):
-        # TODO full implementation (this is only a stub)
         self.__init__()
         with self.__lock:
-            # if len(options) > 0:
-            #     self.__create_options(options)
-            # self._load_options()
-            self.__initialized = True
+            if not self.__initialized:
+                if len(options) > 0:
+                    self.__create_options(options)
+                self._load_options()
+                self.__initialized = True
 
     def add_listener(self, listener: ProtocolListener):
         # TODO implement lock
         if isinstance(listener, ProtocolListener):
             self.__listeners.add(listener)
 
-    @abstractmethod
+    def remove_listener(self, listener: ProtocolListener):
+        # TODO implement lock
+        if isinstance(listener, ProtocolListener):
+            self.__listeners.remove(listener)
+
+    def _internal_reconnect(self) -> bool:
+        self._internal_connect()
+        return True
+
     def _internal_disconnect(self) -> None:
         pass
 
@@ -331,54 +379,95 @@ class Protocol(ABC):
                 self.__connected = False
                 self._internal_disconnect()
 
+    def __reconnect(self) -> None:
+        if self.__reconnect_interval > 0:
+            tick_count = int(round(time.time() * 1000))
+            if tick_count - self.__reconnect_tick_count < self.__reconnect_interval:
+                return
+        try:
+            if self._internal_reconnect():
+                self.__connected = True
+        except Exception as e:
+            raise Exception  # these are swallowed in Jlib
+
+        self.__failed = not self.__connected
+
+        if self.__failed:
+            try:
+                self._reset()
+            except Exception as e:
+                raise Exception  # these are swallowed in Jlib
+
     def __do_reconnect(self) -> None:
         pass
 
-    # def __add_option(self, protocol: str, key: str, value: str) -> None:
-    #     if self.__map_option(key, value):
-    #         return
-    #     if not self._is_valid_option(key):
-    #         raise SmartInspectException(f"Option \"{key}\" is not available for protocol \"{protocol}\"")
-    #
-    #     self.__options.put(key, value)
-    #
-    # def __map_option(self, key: str, value: str) -> bool:
-    #     if key == "backlog":
-    #         self.__options.put(key, value)
-    #         backlog = self.__options.get_size_value("backlog", 0)
-    #
-    #         if backlog > 0:
-    #             self.__options.add("backlog.enabled", "true")
-    #             self.__options.add("backlog.queue", value)
-    #         else:
-    #             self.__options.add("backlog.enabled", "false")
-    #             self.__options.add("backlog.queue", "0")
-    #
-    #             return True
-    #
-    #     if key == "flushon":
-    #         self.__options.put(key, value)
-    #         self.__options.add("backlog.flushon", value)
-    #         return True
-    #
-    #     if key == "keepopen":
-    #         self.__options.put(key, value)
-    #         self.__options.add("backlog.keepopen", value)
-    #         return True
-    #
-    #     return False
-    #
-    # def __remove_options(self) -> None:
-    #     self.__options.clear()
-    #
-    # def _get_level_option(self, key: str, default_value: Level) -> Level:
-    #     return self.__options.get_level_value(key, default_value)
-    #
-    # def _get_boolean_option(self, key: str, default_value: bool) -> bool:
-    #     return self.__options.get_boolean_value(key, default_value)
-    #
-    # def _get_timespan_option(self, key: str, default_value: int) -> int:
-    #     return self.__options.get_timespan_value(key, default_value)
-    #
-    # def _get_size_option(self, key: str, default_value: int) -> int:
-    #     return self.__options.get_size_value(key, default_value)
+    def __add_option(self, protocol: str, key: str, value: str) -> None:
+        if self.__map_option(key, value):
+            return
+        if not self._is_valid_option(key):
+            raise SmartInspectException(f"Option \"{key}\" is not available for protocol \"{protocol}\"")
+
+        self.__options.put(key, value)
+
+    def __map_option(self, key: str, value: str) -> bool:
+        if key == "backlog":
+            self.__options.put(key, value)
+            backlog = self.__options.get_size_value("backlog", 0)
+
+            if backlog > 0:
+                self.__options.add("backlog.enabled", "true")
+                self.__options.add("backlog.queue", value)
+            else:
+                self.__options.add("backlog.enabled", "false")
+                self.__options.add("backlog.queue", "0")
+
+                return True
+
+        if key == "flushon":
+            self.__options.put(key, value)
+            self.__options.add("backlog.flushon", value)
+            return True
+
+        if key == "keepopen":
+            self.__options.put(key, value)
+            self.__options.add("backlog.keepopen", value)
+            return True
+
+        return False
+
+    def __remove_options(self) -> None:
+        self.__options.clear()
+
+    def _get_level_option(self, key: str, default_value: Level) -> Level:
+        return self.__options.get_level_value(key, default_value)
+
+    def _get_boolean_option(self, key: str, default_value: bool) -> bool:
+        return self.__options.get_boolean_value(key, default_value)
+
+    def _get_timespan_option(self, key: str, default_value: int) -> int:
+        return self.__options.get_timespan_value(key, default_value)
+
+    def _get_size_option(self, key: str, default_value: int) -> int:
+        return self.__options.get_size_value(key, default_value)
+
+    def _get_rotate_option(self, key: str, default_value: FileRotate) -> FileRotate:
+        return self.__options.get_rotate_value(key, default_value)
+
+    def _get_bytes_option(self, key: str, size: int, default_value: (bytes, bytearray)) -> (bytes, bytearray):
+        return self.__options.get_bytes_value(key, size, default_value)
+
+    def __schedule_disconnect(self) -> None:
+        command = SchedulerCommand()
+        command.action = SchedulerAction.DISCONNECT
+        self.__scheduler.schedule(command)
+
+    def dispose(self) -> None:
+        self.disconnect()
+
+    @property
+    def failed(self) -> bool:
+        return self.__failed
+
+    @property
+    def _is_asynchronous(self):
+        return self.__async_enabled
