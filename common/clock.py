020bea69cd98be3e0440fca155ebf2afb098668a
Sergey Udachin <sergey.udachin@code-partners.com>
2023-12-17 16:22:58 +0300

    chore: remove unused args

--- common/clock.py
+++ common/clock.py
@@ -4,7 +4,7 @@ from datetime import datetime, timezone
 
 class Clock:
     @staticmethod
-    def now(*args) -> int:
+    def now() -> int:
         current_time_millis = time.time() * 1000
         local_timezone_offset_millis = datetime.now(timezone.utc).astimezone().utcoffset().total_seconds() * 1000
         current_time_micros = int((current_time_millis + local_timezone_offset_millis) * 1000)
