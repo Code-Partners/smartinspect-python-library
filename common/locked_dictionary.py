0219935fcd4437746cfcfa55001bee37ccbd3724
Sergey Udachin <sergey.udachin@code-partners.com>
2023-12-28 12:54:25 +0300

    fix: `LockedDictionary`

--- common/locked_dictionary.py
+++ common/locked_dictionary.py
@@ -1,14 +1,14 @@
 import threading
 
 
-class LockedDictionary:
+class LockedDictionary(dict):
     def __init__(self):
-        self.my_dict = {}
+        super().__init__()
         self.dict_lock = threading.Lock()
 
     def __enter__(self):
         self.dict_lock.acquire()
-        return self.my_dict
+        return self
 
     def __exit__(self, exc_type, exc_value, traceback):
         self.dict_lock.release()
