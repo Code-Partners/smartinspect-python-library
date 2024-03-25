ca1d5b74d66c0fde78e72211f7a6f49c505404c5
Sergey Udachin <sergey.udachin@code-partners.com>
2024-03-25 11:39:23 +0300

    feat: add SmartInspectHandler

--- smartinspect_handler/smartinspect_handler.py
+++ smartinspect_handler/smartinspect_handler.py
@@ -5,6 +5,7 @@ from session.session import Session
 from smartinspect import SmartInspect
 
 
+# noinspection PyBroadException
 class SmartInspectHandler(logging.Handler):
 
     def __init__(self, app_name: str, conn_string: str):
@@ -13,11 +14,21 @@ class SmartInspectHandler(logging.Handler):
         """
 
         logging.Handler.__init__(self)
+
+        self._si: typing.Optional[SmartInspect] = None
         self._si_session: typing.Optional[Session] = None
         self._si_connection_string: str = conn_string
         self._app_name: str = app_name
 
-    # noinspection PyBroadException
+    def get_si(self) -> SmartInspect:
+        """
+        Get underlying SmartInspect instance.
+
+        This will enable SmartInspect if it has not been enabled yet.
+        """
+        self._enable_si()
+        return self._si
+
     def emit(self, record):
         """
         Emit a record.
@@ -27,37 +38,54 @@ class SmartInspectHandler(logging.Handler):
         If a formatter is specified, it is used to format the record.
         The record is then written to destination as defined by SmartInspect session.
         """
-        if not self._si_session:
-            try:
-                self._si_session = self._create_session()
-            except Exception:
-                self.handleError(record)
-
         try:
-            msg = self.format(record)
-            if record.levelno <= 10:
-                self._si_session.log_debug(msg)
-            elif record.levelno <= 20:
-                self._si_session.log_message(msg)
-            elif record.levelno <= 30:
-                self._si_session.log_warning(msg)
-            elif record.levelno <= 40:
-                self._si_session.log_error(msg)
-            else:
-                self._si_session.log_fatal(msg)
-                
+            self._enable_si()
+            self._create_si_session()
+            self._do_emit(record)
         except Exception:
             self.handleError(record)
 
-    def _create_session(self) -> Session:
+    def _enable_si(self) -> None:
         """
-        Create SmartInspect Session using connection string and app_name provided
-        during SmartInspect Handler initialization.
+                Enable SmartInspect instance using connection string and app_name provided
+                during SmartInspect Handler initialization.
         """
-        si = SmartInspect(self._app_name)
-        si.set_connections(self._si_connection_string)
-        si.set_enabled(True)
+        if not self._si or not self._si.is_enabled:
+            try:
+                self._si = SmartInspect(self._app_name)
+                self._si.set_connections(self._si_connection_string)
+                self._si.set_enabled(True)
+            except Exception as e:
+                raise e
 
-        session = si.add_session("Session", True)
+    def _create_si_session(self) -> None:
+        """
+        Create SmartInspect Session used to dispatch logging Records.
+        """
 
-        return session
+        self._si_session = self._si.add_session("Session", True)
+
+    def _do_emit(self, record) -> None:
+        """
+        Emit logging Records to SmartInspect destination protocol according to logging Level.
+        """
+
+        try:
+            msg = self.format(record)
+
+            if record.levelno <= logging.DEBUG:
+                self._si_session.log_debug(msg)
+            elif record.levelno <= logging.INFO:
+                self._si_session.log_message(msg)
+            elif record.levelno <= logging.WARNING:
+                self._si_session.log_warning(msg)
+            elif record.levelno <= logging.ERROR:
+                self._si_session.log_error(msg)
+            else:
+                self._si_session.log_fatal(msg)
+
+        except Exception:
+            self.handleError(record)
+
+    def dispose(self):
+        self._si.dispose()
