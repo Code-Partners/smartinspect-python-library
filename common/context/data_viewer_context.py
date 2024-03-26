ee01ff870fe29b2696a03fd20528471f8acd8e34
Sergey Udachin <sergey.udachin@code-partners.com>
2024-03-26 14:52:29 +0300

    feat: BinViewerContext, DataViewerContext, InspectorViewerContext docs

--- common/context/data_viewer_context.py
+++ common/context/data_viewer_context.py
@@ -3,5 +3,22 @@ from common.viewer_id import ViewerId
 
 
 class DataViewerContext(TextContext):
+    """
+    Represents the data viewer in the Console which can display simple
+    and unformatted text.
+
+    .. note::
+       The data viewer in the Console interprets the data of a Log Entry as text and displays it in a read-only text
+       field.
+
+       You can use the DataViewerContext class for creating custom log
+       methods around LogCustomContext for
+       sending custom text data.
+
+    .. note::
+      This class is not guaranteed to be threadsafe.
+    """
+
     def __init__(self):
+        """Creates and initializes a DataViewerContext instance."""
         super().__init__(ViewerId.DATA)
