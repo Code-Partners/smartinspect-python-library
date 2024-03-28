744256877bfcc21c8f3e62c757978d9f659fdb2b
Sergey Udachin <sergey.udachin@code-partners.com>
2024-03-28 14:38:18 +0300

    feat: FileRotate, FileRotater, Level docs

--- common/file_rotate.py
+++ common/file_rotate.py
@@ -2,6 +2,14 @@ from enum import Enum
 
 
 class FileRotate(Enum):
+    """
+    Specifies the log rotate mode for the FileProtocol class and derived classes.
+        - NO_ROTATE: Completely disables the log rotate functionality.
+        - HOURLY: Instructs the file protocol to rotate log files hourly.
+        - DAILY: Instructs the file protocol to rotate log files daily.
+        - WEEKLY: Instructs the file protocol to rotate log files weekly.
+        - MONTHLY: Instructs the file protocol to rotate log files monthly.
+    """
     NO_ROTATE = 0
     HOURLY = 1
     DAILY = 2
--- common/file_rotater.py
+++ common/file_rotater.py
@@ -5,21 +5,54 @@ from common.file_rotate import FileRotate
 
 
 class FileRotater:
+    """
+    Responsible for the log file rotate management as used by the
+    FileProtocol class.
+
+    This class implements a flexible log file rotate management system.
+    For a detailed description of how to use this class, please refer
+    to the documentation of the initialize, update methods and mode property setter.
+
+    .. note::
+        This class is not guaranteed to be threadsafe.
+    """
     _EPOCH = 1970
     _DAYS_PER_YEAR = 365.2425
     _TIMEZONE = timezone.utc
 
     def __init__(self):
+        """
+        Creates a new FileRotater instance with a default mode of
+        FileRotate.NO_ROTATE. Please refer to the update and initialize
+        methods for additional information about this class.
+        """
         self._calendar: typing.Union[datetime, None] = None
         self._mode: FileRotate = FileRotate.NO_ROTATE
         self._time_value: int = 0
 
     @property
     def mode(self) -> FileRotate:
+        """
+        Returns the FileRotate mode of this FileRotater object.
+        This method returns the current FileRotate mode. For a complete
+        list of available return values, please refer to the FileRotate
+        enum.
+        :return: The FileRotate mode of this FileRotater object.
+        """
         return self._mode
 
     @mode.setter
     def mode(self, rotate_mode: FileRotate) -> None:
+        """
+        Sets the FileRotate mode of this FileRotater object.
+        Always call the initialize method after changing the log rotate
+        mode to reinitialize this FileRotater object. For a complete
+        list of available file log rotate values, please refer to the
+        FileRotate enum.
+
+        :param rotate_mode: The new FileRotate mode.
+        :raises TypeError: if the rotate_mode argument is not FileRotate type.
+        """
         if not isinstance(rotate_mode, FileRotate):
             raise TypeError("rotate mode must be a FileRotate")
         self._mode = rotate_mode
@@ -47,9 +80,25 @@ class FileRotater:
         return time_value
 
     def initialize(self, now: datetime) -> None:
+        """
+        Initializes this FileRotater object with a user-supplied timestamp.
+        Always call this method after creating a new FileRotater object
+        and before calling the update method the first time. For additional
+        information please refer to the update method.
+        :param now: The user-specified timestamp to use to initialize this object.
+        """
         self._time_value = self._get_time_value(now)
 
     def update(self, now: datetime) -> bool:
+        """
+        This method updates the internal date of this FileRotater object and returns
+        whether the rotate state has changed since the last call to this method or to
+        initialize. Before calling this method, always call the initialize method.
+
+        :param now: The timestamp to update this object
+        :returns: True if the rotate state has changed since the last call to this method
+        or to initialize and False otherwise
+        """
         time_value = self._get_time_value(now)
 
         if time_value != self._time_value:
