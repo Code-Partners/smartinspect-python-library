50265945a1e5fe6c71435dcfa3180107d8ae7453
Sergey Udachin <sergey.udachin@code-partners.com>
2024-04-08 11:02:28 +0300

    feat: add docs to SiAuto

--- si_auto.py
+++ si_auto.py
@@ -6,6 +6,35 @@ from smartinspect import SmartInspect
 
 
 class SiAuto:
+    """
+    This class provides automatically created objects
+    for using the SmartInspect and Session classes.
+
+    This class provides a static property called si of type SmartInspect.
+    Furthermore, a Session instance named main with si as parent is ready
+    to use. The SiAuto class is especially useful if you do not want to
+    create SmartInspect and Session instances by yourself.
+
+    The connections string of si is
+    set to "pipe(reconnect=True, reconnect.interval=1s)" for Windows
+    or "tcp(host=localhost, port=4228,reconnect=True, reconnect.interval=1s)" for Linux and Darwin.
+    Application name is set  to "Auto" and the
+    session name of main to "Main".
+
+    The public static members of this class are threadsafe.
+
+    Usage example
+    --------
+        |  SiAuto.si.set_enabled(True)
+        |  SiAuto.main.log_message("Welcome aboard!")
+        |  SiAuto.main.enter_process("SiAutoExample")
+        |  try:
+        |       ...
+        |  finally:
+        |      SiAuto.main.leave_process("SiAutoExample")
+        |      SiAuto.si.dispose()
+    """
+
     _APP_NAME: str = "Auto"
     _WINDOWS_CONNECTION_STRING: str = "pipe(reconnect=True, reconnect.interval=1s)"
     _NON_WINDOWS_CONNECTION_STRING: str = "tcp(host=localhost, port=4228,reconnect=True, reconnect.interval=1s)"
